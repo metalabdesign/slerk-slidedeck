2707b919005695e3b8e8701f0ea8658e410f5d33
CalebLai <caleblai616@gmail.com>
2015-09-30 17:17:18 -0700

    matching ML style

--- presentation/deck.jsx
+++ presentation/deck.jsx
@@ -21,6 +21,40 @@ export default class extends React.Component {
   render() {
     return (
       <Deck transition={["slide"]} transitionDuration={500}>
+        <Slide>
+          <Heading size={1} fit lineHeight={1.2}>
+            Zero to Slack in 30 minutes
+          </Heading>
+          <Heading size={1} fit>
+            An exploration of cutting-edge web technologies
+          </Heading>
+          <Text>By MetaLab</Text>
+        </Slide>
+        <Slide>
+          <Heading>Lorem ipsum</Heading>
+          <CodePane
+            lang="javascript"
+            source={require("raw!./example-code-block")}
+          />
+        </Slide>
+        <Slide>
+          <Heading size={2}>Subtitle here</Heading>
+          <Heading size={5}>Subitlte tag here</Heading>
+        </Slide>
+        <Slide>
+          <Heading size={2}>Slide title</Heading>
+          <Text>Blah blah aosent oaiersnt oiaerns t</Text>
+        </Slide>
+        <Slide transition={["fade"]} bgColor="tertiary" textColor="secondary">
+          <List>
+            <ListItem><Appear fid="1">Inline style based theme system</Appear></ListItem>
+            <ListItem><Appear fid="2">Autofit text</Appear></ListItem>
+            <ListItem><Appear fid="3">Flexbox layout system</Appear></ListItem>
+            <ListItem><Appear fid="4">React-Router navigation</Appear></ListItem>
+            <ListItem><Appear fid="5">PDF export</Appear></ListItem>
+            <ListItem><Appear fid="6">And...</Appear></ListItem>
+          </List>
+        </Slide>
       </Deck>
     );
   }
