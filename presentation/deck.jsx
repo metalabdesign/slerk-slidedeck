4c5216bc9ad98a09a35db46a6944eada1ebba0e2
James Phillips <jamesdphillips@gmail.com>
2015-10-28 17:17:51 -0700

    WIP Things ... and stuff!

--- presentation/deck.jsx
+++ presentation/deck.jsx
@@ -1,7 +1,7 @@
 import React from "react/addons";
 
 import {
-  Appear, BlockQuote, Cite, CodePane, Deck, Fill,
+  Appear, BlockQuote, Cite, Code, CodePane, Deck, Fill,
   Heading, Image, Layout, Link, ListItem, List, Quote, Slide, Text
 } from "../src/spectacle";
 
@@ -11,9 +11,12 @@ import styles from '../themes/metalab/index';
 
 const images = {
   metalab: require("./images/metalab.png"),
-  slerkScreenshot: require("./images/slerk-screenshot.png"),
-  reactSlide: require("./images/react-slide.jpg"),
-  smartVDumbComponents: require("./images/smart-v-dumb-components.png"),
+  graphql: require("./images/graphql.svg"),
+  netscape: require("./images/netscape.png"),
+  restfulFlow: require("./images/restful-flow.png"),
+  graphqlFlow: require("./images/graphql-flow.png"),
+  shape: require("./images/gql-shape.png"),
+  yupItsAGraph: require("./images/a-graph.png")
 };
 
 preloader([images.slerkScreenshot, images.reactSlide, images.smartVDumbComponents]);
@@ -28,156 +31,257 @@ function sectionTitleSlide(title, subtitle) {
   );
 }
 
+const restful = `
+  CRUD /:resource/:identifier
+
+  GET /tasks
+  GET /users/me
+
+  POST /tasks
+  PUT /messages/123
+
+  DELETE /competitors
+  DELETE /article/last-exit-to-springfield
+`;
+
+const customEndpoint = `
+/* ex. GET /tasks/1
+ *
+ * - includes all data required to render the view
+ * - given a clients requirements may return too much or too little data
+ * - leads to lots of churn as client requirements change & new features added
+ */
+
+{
+  "task": {
+    "text": "You tried your best and you failed miserably. The lesson is, never try.",
+    ...,
+    "creator": {
+      "id": "abc",
+      "name": "James",
+      ...
+    }
+    "activities": [
+      ...
+    ]
+  }
+}
+`;
+
+const exGraphQuery = `
+query FetchTasks {
+  tasks(id: 57) {
+
+    title
+    due_on
+
+    activities {
+      text
+      actor {
+        name
+        avatar
+      }
+    }
+
+    # so on..
+  }
+}
+`;
+
+const exampleFrag= `
+fragment userDetails on User {
+  name
+  nickname
+  online
+  channels {
+    name
+  }
+}
+`;
+
+const exampleMutation= `
+mutation UpdateTask {
+  updateTask(completed: true) {
+    task {
+      completed
+    }
+    activity {
+      id
+      title
+      payload {
+        # ...
+      }
+    }
+  }
+}
+`;
+
+const exampleSubscription= `
+subscription ExmapleSubscription {
+  taskCompleted(ID: "123") {
+    task { completed }
+    activity { id, title }
+  }
+}
+`;
+
+const exampleLive= `
+query LiveQuery {
+  @live
+  task(id: "15") {
+    title 
+    completed
+    tags
+    subscribers {
+      # ...
+    }
+  }
+}
+`;
+
 export default class extends React.Component {
   render() {
     return (
       <Deck transition={["slide"]} transitionDuration={200}>
 
         <Slide>
-          <Heading size={4} fit>
-            Zero to Slack in 30 minutes
-          </Heading>
-          <Text>An exploration of modern web development</Text>
+          <Heading size={1} fit>GraphQL</Heading>
+          <Text>[🍕 & 🤓] Nov. 15'</Text>
         </Slide>
 
         <Slide>
-          <Heading size={4}>Hi there</Heading>
-          <Text>We're Izaak, James, and Jason from MetaLab</Text>
-          <Link href="https://metalab.co"><Image style={styles.MLlogo}  height="200px" width="200px" src={images.metalab}/></Link>
+          <Heading size={1}>REST</Heading>
+          <Text>A monument to all our sins.</Text>
         </Slide>
 
         <Slide>
-          <Heading size={4}>Let's build Slerk™</Heading>
-          <Text>A real time multiuser chat application</Text>
-          <Image src={images.slerkScreenshot.replace("/", "")} style={styles.slerkScreenshot}/>
-        </Slide>
-
-        <Slide>
-          <Heading size={4}>Setup</Heading>
-          <Text>Go to repo, follow instructions</Text>
-          <Text>
-            <Link href="https://github.com/metalabdesign/startupslam-workshop">
-              https://github.com/metalabdesign/startupslam-workshop
-            </Link>
-          </Text>
-        </Slide>
-
-        <Slide>
-          <Heading size={4}>Main technologies</Heading>
           <List>
-            <ListItem>
-              <Link href="http://elixir-lang.org/">Elixir</Link>
-              {" & "}
-              <Link href="http://www.phoenixframework.org/">Phoenix</Link>
-            </ListItem>
-            <ListItem>
-              <Link href="https://babeljs.io/">Babel</Link>
-              {' '}
-              (<Link href="http://es6-features.org/">ES6/2015</Link>)
-            </ListItem>
-            <ListItem>
-              <Link href="https://webpack.github.io/">Webpack</Link> (HMR, CSS Modules)
-            </ListItem>
-            <ListItem>
-              <Link href="https://github.com/rackt/redux">Redux</Link> (&amp; devtools)
-            </ListItem>
-            <ListItem>
-              <Link href="https://facebook.github.io/react/">React</Link>
-            </ListItem>
+            <ListItem>Request a single resource or collection of resources.</ListItem>
+            <ListItem>Very simple.</ListItem>
+          </List>
+          <CodePane lang="bash" source={restful} />
+        </Slide>
+
+        <Slide>
+          <Image src={images.netscape} height="30vw" />
+        </Slide>
+
+        <Slide>
+          <Image src={images.restfulFlow} height="35vw" fit/>
+        </Slide>
+
+        <Slide>
+          <Image src={images.yupItsAGraph} height="35vw" fit/>
+        </Slide>
+
+        <Slide>
+          <List>
+            <ListItem>Want to avoid second, third hop to fetch data.</ListItem>
+            <ListItem>Each client has different data requirements.</ListItem>
+            <ListItem>This leads us to complex endpoints.</ListItem>
+          </List>
+          <CodePane lang="javascript" source={customEndpoint} />
+        </Slide>
+
+        <Slide>
+          <Heading size={1} style={{"font-size": "300px"}}>💩 </Heading>
+        </Slide>
+
+        <Slide>
+          <Image src={images.graphql} height="300px" />
+          <Heading size={1} fit>GraphQL</Heading>
+          <Text>Its divine wind will rush through the stars.</Text>
+        </Slide>
+
+        <Slide>
+          <Heading size={4}>Wat is it?</Heading>
+          <List>
+            <ListItem>Product-centric query language</ListItem>
+            <ListItem>Declaritively request data from the server</ListItem>
+            <ListItem>Trivializes fetching dependencies</ListItem>
+            <ListItem>Legible syntax</ListItem>
+          </List>
+          {/*<CodePane lang="json" source={exGraphQuery} />*/}
+        </Slide>
+
+        <Slide>
+          <Heading size={4}>Wat does it look like</Heading>
+          <List>
+            <ListItem>Similar to Map syntax in many languages</ListItem>
+            <ListItem>Response matches the shape of the request</ListItem>
+            <ListItem>Hierarchical</ListItem>
+          </List>
+          <Image src={images.shape} height="20vw"/>
+        </Slide>
+
+        <Slide>
+          <Heading size={4}>A Schema?!</Heading>
+          <List>
+            <ListItem>Introspection</ListItem>
+            <ListItem>Strongly-typed</ListItem>
+            <ListItem>Useful in statically typed languages</ListItem>
+            <ListItem>Built-in documentation</ListItem>
+            <ListItem>Version-free</ListItem>
           </List>
         </Slide>
 
+        {/*
         <Slide>
-          <Heading size={4}>Elixir &amp; Phoenix</Heading>
+          <Image src={images.graphqlFlow} height="35vw" />
+        </Slide>
+        */}
+
+        <Slide>
+          <Heading size={1}>Demo</Heading>
+        </Slide>
+
+        <Slide>
+          <Heading size={4}>Fragments</Heading>
           <List>
-            <ListItem>Elixir: targets Erlang VM, looks like Ruby</ListItem>
-            <ListItem>Concurrent, immutable, functional</ListItem>
-            <ListItem>Phoenix: web framework for Elixir (Rails-ish)</ListItem>
-            <ListItem>Built-in websocket channels (good for Slerk)</ListItem>
+            <ListItem>Composable query 'fragments'</ListItem>
+            <ListItem>Build query from fragments</ListItem>
+            <ListItem>Typically views declare a fragment</ListItem>
           </List>
+          <CodePane lang="json" source={exampleFrag}/>
         </Slide>
 
         <Slide>
-          <Heading size={4}>Babel, ES6/2015</Heading>
-          <Text>Tomorrow's JavaScript today!</Text>
-          <CodePane lang="bash" source={require("raw!./code/es6-examples-1")}/>
-        </Slide>
-
-        <Slide>
-          <Heading size={4}>Babel, ES6/2015 (2)</Heading>
-          <CodePane lang="bash" source={require("raw!./code/es6-examples-2")}/>
-        </Slide>
-
-        <Slide>
-          <Heading size={4}>Webpack</Heading>
+          <Heading size={4}>Mutations</Heading>
           <List>
-            <ListItem>Asset build + compilation system</ListItem>
-            <ListItem>Source files, runs transforms, builds assets</ListItem>
-            <ListItem>Huge list of plugins/transforms/etc</ListItem>
-            <ListItem>React HMR - hot patches JS without reloads</ListItem>
+            <ListItem>Oparation to manipulate remote state</ListItem>
+            <ListItem>Declaratively fetch data from response</ListItem>
+            <ListItem>So called 'fat queries' to fetch any side-effects</ListItem>
           </List>
+          <CodePane lang="json" source={exampleMutation}/>
         </Slide>
 
         <Slide>
-          <Heading size={4}>Redux</Heading>
+          <Heading size={4}>Subscriptions</Heading>
           <List>
-            <ListItem>"Predictable state mangement for JS apps"</ListItem>
-            <ListItem>Actions → reducers → store</ListItem>
-            <ListItem>React components render based on store</ListItem>
-            <ListItem>Only 99 lines without guards/checks</ListItem>
-            <ListItem>Rich debugging tools</ListItem>
+            <ListItem>Oparation to subscribe to realtime updates</ListItem>
+            <ListItem>Very similar to mutations</ListItem>
+            <ListItem>Declaratively fetch data from payload</ListItem>
+            <ListItem><Link>https://github.com/facebook/graphql/pull/109</Link></ListItem>
+            <ListItem><Link>https://github.com/facebook/relay/issues/541</Link></ListItem>
           </List>
+          <CodePane lang="json" source={exampleSubscription}/>
         </Slide>
 
         <Slide>
-          <Heading size={4}>Last, but not least...</Heading>
-        </Slide>
-
-        <Slide bgImage={images.reactSlide.replace("/", "")}></Slide>
-
-        <Slide>
-          <Heading size={4}>React is...</Heading>
+          <Heading size={4}>Live Queries</Heading>
           <List>
-            <ListItem>Declarative view library for UIs</ListItem>
-            <ListItem>Used as the V in MV*</ListItem>
-            <ListItem>Made by Facebook, in production many places</ListItem>
-            <ListItem>tite</ListItem>
+            <ListItem>RFC for @live directive</ListItem>
+            <ListItem>Define queries that change over time</ListItem>
+            <ListItem>Possible with: Datomic, RethinkDB, others.</ListItem>
+            <ListItem><Link>https://github.com/graphql/graphql/issues/188</Link></ListItem>
           </List>
+          <CodePane lang="json" source={exampleLive}/>
         </Slide>
 
         <Slide>
-          <Heading size={4}>Why React?</Heading>
-          <List>
-            <ListItem>Encapsulates data, markup, and behaviour</ListItem>
-            <ListItem>Declarative view structure</ListItem>
-            <ListItem>Efficient rendering</ListItem>
-            <ListItem>Plays well with immutable data structures</ListItem>
-            <ListItem>Server-side rendering (isomorphic applications)</ListItem>
-            <ListItem>React Native for iOS, Android</ListItem>
-          </List>
+          <Heading size={1} fit>Thanks!</Heading>
         </Slide>
 
-        <Slide>
-          <Heading size={4}>React Components</Heading>
-          <List>
-            <ListItem>Components ecapsulate data and behaviour, and produce markup</ListItem>
-            <ListItem>Data: properties and state</ListItem>
-            <ListItem>Referentially transparent</ListItem>
-            <ListItem>Whenever props or state change, component(s) re-render</ListItem>
-            <ListItem>Efficient renders due to virtual DOM + diffing</ListItem>
-            <ListItem>Nestable, composable</ListItem>
-          </List>
-        </Slide>
-
-        <Slide>
-          <Heading size={4}>Smart/Dumb Components</Heading>
-          <Text>Most components should be dumb, only props</Text>
-          <Image margin="30px auto 40px" width="50vw" src={images.smartVDumbComponents.replace("/", "")}/>
-        </Slide>
-
-        <Slide>
-          <Heading size={4}>Let's code!</Heading>
-        </Slide>
 
       </Deck>
     );
